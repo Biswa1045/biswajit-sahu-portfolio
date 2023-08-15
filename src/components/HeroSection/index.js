ee343bc1f5acab1a381c6932a69e5aba211b1361
biswa1045 <biswajitsahu1045@gmail.com>
2023-08-15 15:47:17 +0530

    initial commit

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import HeroBgAnimation from '../HeroBgAnimation'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
+import HeroImg from '../../images/HeroImage.jpg'
+import Typewriter from 'typewriter-effect';
+import { Bio } from '../../data/constants';
+
+const HeroSection = () => {
+    return (
+        <div id="about">
+            <HeroContainer>
+                <HeroBg>
+                    <HeroBgAnimation />
+                </HeroBg>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <Title>Hi, I am <br /> {Bio.name}</Title>
+                        <TextLoop>
+                            I am a
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: Bio.roles,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </Span>
+                        </TextLoop>
+                        <SubTitle>{Bio.description}</SubTitle>
+                        <ResumeButton href={Bio.resume} target='display'>Check Resume</ResumeButton>
+                    </HeroLeftContainer>
+
+                    <HeroRightContainer id="Right">
+
+                        <Img src={"https://media.licdn.com/dms/image/D5603AQH3peDrI5cNqA/profile-displayphoto-shrink_800_800/0/1685460687516?e=1697673600&v=beta&t=UEPb0LGMCj9SxfR1q0z0YSmkEHOX7AbLaZPvxlYNe7o"} alt="hero-image" />
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+
+            </HeroContainer>
+        </div>
+    )
+}
+
+export default HeroSection
